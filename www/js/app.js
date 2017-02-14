3e8e4e4de174688d70ac6af7c21e9482efe50860
pravin.gaikwad@clariontechnologies.co.in <pravin.gaikwad@clariontechnologies.co.in>
2017-02-14 19:38:07 +0530

    New

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', 'ngStorage'])
 
 .run(function($ionicPlatform) {
+  
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,14 +22,15 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+  
 })
 
-.config(function($stateProvider, $urlRouterProvider){
+.config(function($stateProvider, $urlRouterProvider, $httpProvider){
   $stateProvider
   .state('app', {
     url:'/app',
     templateUrl: 'templates/menu.html',
-    // controller: 'AppCtrl',
+    controller: 'AppCtrl',
     abstract: true
   })
   .state('app.home', {
@@ -36,9 +38,22 @@ angular.module('starter', ['ionic'])
     views: {
       'menuContent':{
         templateUrl: 'templates/home.html',
-        // controller: 'HomeCtrl'
+         controller: 'HomeCtrl'
       }
     }
-  });
+  })
+   .state('app.browse', {
+    url:'/browse',
+    views: {
+      'menuContent':{
+        templateUrl: 'templates/browse.html',
+         controller: 'BrowseCtrl'
+      }
+    }
+  })
   $urlRouterProvider.otherwise('/app/home');
+  $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common = 'Content-Type: application/json';
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
 })
+
---
+++ www/js/controllers.js
@@ -0,0 +1,71 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, WC){
+  var Woocommerce = WC.WC();
+
+  Woocommerce.get('products/categories', function(err, data, res){
+   
+    
+    //console.log(res);
+    $scope.categories = (JSON.parse(res)).product_categories;
+
+    // $scope.mainCategories = [];
+    // $scope.categories.forEach(function(element, index){
+    //   if(element.parent == 0)
+    //   $scope.mainCategories.push(element);
+    // })
+  })
+})
+
+.controller('HomeCtrl', function(){
+
+
+})
+
+.controller('BrowseCtrl', function($scope, WC){
+    $scope.offset = 0;
+    $scope.getProducts = function(){
+      var Woocommerce = WC.WC();
+
+      Woocommerce.get('products', function(err, data, res){
+        if(err)
+          console.log(err);
+
+          console.log(JSON.parse(res));
+
+
+          $scope.products = JSON.parse(res).products;
+          $scope.offset = $scope.offset + 10;
+          $scope.canLoadMore = true;
+      })
+    }
+    $scope.getProducts();
+    $scope.doRefresh = function(){
+      $scope.getProducts();
+      
+      $scope.$broadcast('scroll.refreshComplete');
+    }
+    $scope.loadMore = function(){
+      var Woocommerce = WC.WC();
+
+      Wocommerce.get('products?filter[offset]='+$scope.offset, function(err, data, res){
+          if(err)
+            console.log(err);
+
+            JSON.parse(res).products.forEach(function(){
+              $scope.products.push(element);
+            })
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+
+            if(JSON.parse(res).products.length < 10){
+              $scope.canLoadMore = false;
+              console.log('no more products!');
+              return;
+            }
+            else{
+              $scope.offset = $scope.offset + 10;
+            }
+      })
+    }
+
+})
