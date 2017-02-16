83d8f2b03cd2d0309c2c4a57667deb8a7a560511
Pravin <pravinngkd@gmail.com>
2017-02-16 22:46:12 +0530

    New

--- www/js/app.js
+++ www/js/app.js
@@ -3,10 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', 'ngStorage'])
+angular.module('starter', ['ionic','starter.services','starter.controllers','ngStorage'])
 
 .run(function($ionicPlatform) {
-  
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -22,38 +21,89 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers', '
       StatusBar.styleDefault();
     }
   });
-  
 })
 
-.config(function($stateProvider, $urlRouterProvider, $httpProvider){
+.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
   .state('app', {
-    url:'/app',
+    url: '/app',
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl',
     abstract: true
   })
   .state('app.home', {
-    url:'/home',
-    views: {
-      'menuContent':{
+    url : '/home',
+    views : {
+      'menuContent' : {
         templateUrl: 'templates/home.html',
-         controller: 'HomeCtrl'
+        controller: 'HomeCtrl'
       }
     }
   })
-   .state('app.browse', {
-    url:'/browse',
-    views: {
-      'menuContent':{
+  .state('app.browse', {
+    url : '/browse',
+    views : {
+      'menuContent' : {
         templateUrl: 'templates/browse.html',
-         controller: 'BrowseCtrl'
+        controller: 'BrowseCtrl'
       }
     }
   })
+  .state('app.categories', {
+    url : '/categories',
+    views : {
+      'menuContent' : {
+        templateUrl: 'templates/categories.html',
+        controller: 'CategoriesCtrl'
+      }
+    }
+  })
+  .state('app.category', {
+    url : '/categories/:categoryID',
+    views : {
+      'menuContent' : {
+        templateUrl: 'templates/category.html',
+        controller: 'CategoryCtrl'
+      }
+    }
+  })
+  .state('app.product', {
+    url : '/product/:productID',
+    views : {
+      'menuContent' : {
+        templateUrl: 'templates/product.html',
+        controller: 'ProductCtrl'
+      }
+    }
+  })
+  .state('app.signup', {
+    url : '/signup/',
+    views : {
+      'menuContent' : {
+        templateUrl: 'templates/signup.html',
+        controller: 'SignUpCtrl'
+      }
+    }
+  })
+  .state('app.login', {
+    url : '/login/',
+    views : {
+      'menuContent' : {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  })
+  .state('app.checkout', {
+    url : '/checkout/',
+    views : {
+      'menuContent' : {
+        templateUrl: 'templates/checkout.html',
+        controller: 'CheckoutCtrl'
+      }
+    }
+  });
+  
   $urlRouterProvider.otherwise('/app/home');
-  $httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.headers.common = 'Content-Type: application/json';
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
 })
 
